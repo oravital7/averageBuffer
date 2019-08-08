c09c18bc4536be8f7acb9695379e8b099cd82de9
oravital7 <oravital7@gmail.com>
2019-08-08 23:54:42 +0300

    Minors bug change

--- average_buffer.hpp
+++ average_buffer.hpp
@@ -1,111 +1,119 @@
-#include <iostream>
-
 #define QUARTER 4
 
 template <typename T>
 class AverageBuffer
 {
+public:
     T *buffer;
-    int bufferSize, currIndex, upperIndex, lowerIndex, numForever; // The idea to keep the average and not the whole amount prevent overflow
+    int bufferSize, currIndex, upperIndex, lowerIndex, numForever;
     double BufferAvg, foreverAvg, upperAvg, lowerAvg;
 
 public:
     AverageBuffer(int bufferSize) : buffer(new T[bufferSize]), bufferSize(bufferSize)
     {
-        foreverAvg = BufferAvg = currIndex = numForever = 0;
-        upperAvg = lowerAvg = 0;
-        upperIndex = lowerIndex = 0;
-        // Init method
+        initializeGlobal();
     }
 
     ~AverageBuffer() { delete[] buffer; }
 
-    double getAverage()
+    double getAverage() const
     {
         return BufferAvg;
     }
 
-    double getAverageForever()
+    double getAverageForever() const
     {
         return foreverAvg;
     }
 
-    double getUpperQuarterAverage() {
+    double getUpperQuarterAverage() const
+    {
         return upperAvg;
     }
-    
-    double getLowerQuarterAverage() {
+
+    double getLowerQuarterAverage() const
+    {
         return lowerAvg;
     }
 
-    void addSample(T number)
+    void addSample(const T &newVal)
     {
         updateLowerAverage();
-        updateUpperAverage(number);
-        updateAverages(number);
- 
-        buffer[currIndex] = number;
-        circleIndex(currIndex);
-        for(int i=0; i< bufferSize; i++)
-          std::cout <<  buffer[i] << " , ";
+        updateUpperAverage(newVal);
+        updateAverages(newVal);
 
-          std::cout << std::endl;
-    }
-// Try change ifs
-private:
-    void updateUpperAverage(T newVal) {
-        if(numForever < bufferSize){
-            if((numForever+1) % QUARTER == 0) {  // We need to add new value to avearge
-                upperAvg =  (upperAvg * (numForever / QUARTER) + newVal) / ((numForever + 1) / QUARTER);
-            } else if((numForever+1) /  QUARTER != 0) {  
-              upperAvg += (newVal - buffer[upperIndex]) / ((numForever+1) /  QUARTER);
-                circleIndex(upperIndex);
-            }
-            else { // stay on 0 while there is no quarter yet
-                circleIndex(upperIndex);
-            }
-
-        } else {
-            upperAvg += (newVal - buffer[upperIndex]) / (bufferSize /  QUARTER);
-            circleIndex(upperIndex);
-        }
-
+        buffer[currIndex] = newVal;
+        circularIndex(currIndex);
     }
 
-    void updateLowerAverage() {
-         if(numForever < bufferSize){
-            if((numForever+1) % QUARTER == 0) {
-                lowerAvg =  (lowerAvg * (numForever / QUARTER) + buffer[lowerIndex]) / ((numForever + 1) / QUARTER);
-                circleIndex(lowerIndex);
-            }
-
-        } else {
-            std::cout << "yes" << std::endl;
-            lowerAvg += (buffer[lowerIndex] - buffer[currIndex]) / (bufferSize /  QUARTER);
-            circleIndex(lowerIndex);
-        }
-
-
-    }
-
-
-    void updateAverages(T newVal)
+    void clear()
     {
-        if (numForever < bufferSize) // Calculate first values only 
+        initializeGlobal();
+        delete[] buffer; // The question require to clear the buffer, delete seems more effective
+        buffer = new T[bufferSize];
+    }
+
+private:
+    void updateUpperAverage(const T &newVal)
+    {
+        if (numForever < bufferSize)
         {
-            foreverAvg = BufferAvg = (foreverAvg * numForever + newVal) / (numForever + 1);
-        } else  {
-            BufferAvg += (newVal - buffer[currIndex]) / bufferSize;
-            foreverAvg = (foreverAvg * numForever + newVal) / (numForever + 1);
+            if ((numForever + 1) % QUARTER == 0) // Need add new value to Upper Average
+            {
+                upperAvg = (upperAvg * (numForever / QUARTER) + newVal) / ((numForever + 1) / QUARTER);
+            }
+            else if ((numForever + 1) / QUARTER != 0) // Need add and replace old value by new value to Upper Average
+            {
+                upperAvg += (newVal - buffer[upperIndex]) / ((numForever + 1) / QUARTER);
+                circularIndex(upperIndex);
+            }
+            else
+            { // There is no quarter yet
+                circularIndex(upperIndex);
+            }
         }
+        else // Need add and replace old value by new value to Upper Average
+        {
+            upperAvg += (newVal - buffer[upperIndex]) / (bufferSize / QUARTER);
+            circularIndex(upperIndex);
+        }
+    }
+
+    void updateLowerAverage()
+    {
+        if (numForever < bufferSize)
+        {
+            if ((numForever + 1) % QUARTER == 0) // Need add new value to Lower Average
+            {
+                lowerAvg = (lowerAvg * (numForever / QUARTER) + buffer[lowerIndex]) / ((numForever + 1) / QUARTER);
+                circularIndex(lowerIndex);
+            }
+        }
+        else // Need add and replace old value by new value to Lower Average
+        {
+            lowerAvg += (buffer[lowerIndex] - buffer[currIndex]) / (bufferSize / QUARTER);
+            circularIndex(lowerIndex);
+        }
+    }
+
+    // Update Buffer and forever averages
+    void updateAverages(const T &newVal)
+    {
+        foreverAvg = (foreverAvg * numForever + newVal) / (numForever + 1);
+        BufferAvg = numForever < bufferSize ? foreverAvg : BufferAvg + (newVal - buffer[currIndex]) / bufferSize;
         numForever++;
     }
 
-    void circleIndex(int &index) {
-        index = (index+1) % bufferSize;
+    // Move the index in a circular way
+    void circularIndex(int &index)
+    {
+        index = (index + 1) % bufferSize;
     }
 
-    double nextAverage(T newVal) {
-        return (foreverAvg * numForever + newVal) / (numForever + 1);
+    void initializeGlobal()
+    {
+        foreverAvg = BufferAvg = upperAvg = lowerAvg = 0;
+        currIndex = upperIndex = lowerIndex = 0;
+        numForever = 0;
     }
 };
